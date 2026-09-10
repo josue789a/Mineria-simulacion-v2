8a65d9d313f100831f2e95ae57a5a09932841736
Josue <jbardalesgamboa@gmail.com>
2026-09-10 18:46:15 -0500

    EDA 'detectar_fecha' mejorado

--- src/eda.py
+++ src/eda.py
@@ -2,7 +2,6 @@
 
 
 ##1) FUNCION PARA VERIFIZAR LIMPIEZA BASICA DE DATAFRAMES
-
 import pandas as pd
 
 
@@ -40,25 +39,26 @@ def reporte_calidad(df, nombre_df=""):
  ## Aplica un formato al valor asignado, por ejemplo value es YYYYMMDD sera 'YMD', si por el contrario es DDMMYYY sera 'DMY', por tanto aplicarlo sobre una columna, crearia una nueva columna
 
 import re
-
 def detectar_fecha(valor):
-    """Detecta el formato estructural de una fecha."""
 
     if pd.isna(valor):
         return None
 
     valor = str(valor).strip()
 
-    # YYYY-MM-DD / YYYY/MM/DD / YYYY.MM.DD / YYYY MM DD
-    if re.fullmatch(r'\d{4}[-/. ]\d{1,2}[-/. ]\d{1,2}', valor):
+    if re.fullmatch(r'\d{4}-\d{1,2}-\d{1,2}', valor):
         return 'YMD'
 
-    # DD-MM-YYYY / DD/MM/YYYY / DD.MM.YYYY / DD MM YYYY
-    if re.fullmatch(r'\d{1,2}[-/. ]\d{1,2}[-/. ]\d{4}', valor):
+    if re.fullmatch(r'\d{4}/\d{1,2}/\d{1,2}', valor):
+        return 'YMD_SLASH'
+
+    if re.fullmatch(r'\d{1,2}-\d{1,2}-\d{4}', valor):
         return 'DMY'
 
-    # YYYY-MM-DD HH:MM:SS
-    if re.fullmatch(r'\d{4}[-/. ]\d{1,2}[-/. ]\d{1,2}.*', valor):
+    if re.fullmatch(r'\d{1,2}/\d{1,2}/\d{4}', valor):
+        return 'DMY_SLASH'
+
+    if re.fullmatch(r'\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}', valor):
         return 'YMD_TIME'
 
     return 'DESCONOCIDO'
